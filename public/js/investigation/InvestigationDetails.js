90f2a10f761d944ea56105be6e42d03dcb144711
dferreto <diferretomo@est.utn.ac.cr>
2023-12-04 02:52:53 -0600

    diseño de los detalles de investigacion

--- public/js/investigation/InvestigationDetails.js
+++ public/js/investigation/InvestigationDetails.js
@@ -25,7 +25,7 @@ function cargarDetallesInvestigacion(id) {
                 db.collection("datosUsuarios").where("idemp", "==", datosInvestigacion.userId).get().then(querySnapshot => {
                     if (!querySnapshot.empty) {
                         const datosUsuario = querySnapshot.docs[0].data();
-                        detallesContainer.innerHTML = construirDetallesInvestigacion(datosInvestigacion, datosUsuario.gradoAcademico);
+                        detallesContainer.innerHTML = construirDetallesInvestigacion(datosInvestigacion, datosUsuario.gradoAcademico, datosUsuario.email);
                     } else {
                         detallesContainer.innerHTML = '<p>Detalles del usuario no encontrados para la investigación con ID: ' + id + '.</p>';
                     }
@@ -45,12 +45,75 @@ function cargarDetallesInvestigacion(id) {
     });
 }
 
-function construirDetallesInvestigacion(datosInvestigacion, gradoAcademico) {
+function construirDetallesInvestigacion(datosInvestigacion, gradoAcademico, email) {
     return `
         <h2>${datosInvestigacion.titulo}</h2>
-        <p>${datosInvestigacion.descripcion}</p>
-        <p>Área de interés: ${datosInvestigacion.area}</p>
-        <p>Grado académico: ${gradoAcademico || 'Información no disponible'}</p>
-        <!-- Agrega aquí más campos si son necesarios -->
+ 
+        <div class="container-fluid mt-4">
+        <div class="row">
+          <!-- DISCIPLINA Column -->
+          <div class="col-md-3 d-flex flex-column">
+            <div class="card shadow">
+              <div class="card-header alert-link ">INFORMACIÓN GENERAL</div>
+              <div class="card-body">
+                <p>Área de interés: ${datosInvestigacion.area}</p>
+                <p>Grado académico: ${gradoAcademico || 'Información no disponible'}</p>
+              </div>
+            </div>
+            <div class="card flex mt-4 shadow fade-in-cardMiddle">
+                <div class="card-header alert-link">INVESTIGACIÓN</div>
+                <div class="card-body">
+                    <img src="img/listInvest/pdf.png" alt="PDF" class="categoria-imagen"> 
+                    <!-- More content can be added here -->
+                </div>
+            </div>
+            <div class="card flex-fill mt-4 shadow fade-in-cardMiddle">
+                <div class="card-header alert-link">DESCRIPCIÓN</div>
+                <div class="card-body">
+                <p>${datosInvestigacion.descripcion}</p>
+                  
+                </div>
+            </div>
+            <div class="card flex mt-4 shadow fade-in-cardMiddle">
+                <div class="card-header alert-link">INFORMACIÓN DE CONTACTO:</div>
+                <div class="card-body">
+                <p>Email: ${email || 'Información no disponible'}</p>
+                </div>
+            </div>
+          </div>
+          
+          <!-- MAIN CONTENT Middle Column, ajustado a col-md-6 para totalizar 12 columnas en la fila -->
+          <div class="col-md-6 d-flex-fill flex-column">
+            <!-- Existing Card -->
+            <div class="card mb-3 shadow">
+              <img src="img/principal/circulo1.jpeg" class="card-img-top img-fluid" alt="Descripción de la imagen">
+              
+              <!-- Image content here -->
+            </div>
+          </div>
+          
+          <!-- Right Column, ajustado a col-md-3 para igualar al de la DISCIPLINA Column -->
+<div class="col-md-3">
+<div class="d-flex flex-column h-100">
+  <!-- CONCLUSIÓN Card -->
+  <div class="card shadow mb-2 flex-fill"> <!-- mb-2 para un pequeño margen entre los cards -->
+    <div class="card-header alert-link">CONCLUSIÓN:</div>
+    <div class="card-body">
+      <!-- Content here -->
+    </div>
+  </div>
+  <!-- RECOMENDACIÓN Card -->
+  <div class="card shadow flex-fill mt-3">
+    <div class="card-header alert-link">RECOMENDACIÓN:</div>
+    <div class="card-body">
+      GFDGGGGGGGGG
+    </div>
+  </div>
+</div>
+</div>
+
+          
+        </div>
+      </div>
     `;
 }
